dd443d403d5b1fd999cfb20df9f378e1b18e79df
Colin Burr <colin@colin.id>
2024-02-04 20:24:20 -0500

    Update Dockerfiles/config for deployment

--- app/bookmark/mix.exs
+++ app/bookmark/mix.exs
@@ -45,6 +45,7 @@ defmodule Bookmark.MixProject do
       {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
+      {:gen_smtp, "~> 1.2.0"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
